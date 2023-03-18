cfaa4c2abb6e99aa1a09fa2a864213b8ead7bfc0
cerina Malaluan <cerinamalaluan@gmail.com>
2023-03-18 11:59:07 -0700

    chatty cats logo

--- src/components/fqa.js
+++ src/components/fqa.js
@@ -1,4 +1,11 @@
 import React from "react"
+import logo from "/Users/cerinamalaluan/Projects/milestone-project-2-1/Assests/logo.png";
+console.log(logo); // /logo.png
+
+function Header() {
+  // hopefully our cat logo
+  return <img src={logo} alt="Logo" />;
+}
 
 function FQA () {
 
@@ -7,7 +14,13 @@ function FQA () {
         <h3>this is fqa</h3>
         <h4>Why chattkat</h4>
         <p>
-            who doesnt love cats?
+            who doesnt love cats? Meet our chatty cats! <div id="myImg"></div>
+            <script>
+                var img = document.createElement('img');
+                img.src = "/Users/cerinamalaluan/Projects/milestone-project-2-1/Assests/The-chattty-cats.png";
+                var src = document.getElementById("myImg);
+                src.appendChild(img);
+                 </script>
         </p>
         <h4>Who made this?</h4>
         <p>
