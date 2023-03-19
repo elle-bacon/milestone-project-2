08bc0c35ce2b924af4715e3f9b9f4cd9fb46cd67
Emily Bacon <em222ilyb@gmail.com>
2023-03-19 13:08:30 -0700

    fqa and landing page

--- src/components/fqa.js
+++ src/components/fqa.js
@@ -1,23 +1,36 @@
 import React from "react"
 
-
 function FQA () {
 
     return(
         <>
-        <h3>this is fqa</h3>
-        <h4>Why chattkat</h4>
+        <h3>Welcome to FQA (Frequent Questions Asked.)</h3>
+        <h4>Why ChattyKat?</h4>
         <p>
-            who doesnt love cats? Meet our chatty cats!
+            Who doesnt love cats?
         </p>
         <h4>Who made this?</h4>
         <p>
-            Emily, Daniel and Cerina as a milestone project for a software development bootcamp!
+            Emily, Daniel and Cerina created this as a milestone project for a software development bootcamp!
         </p>
         <h4>What does this website do?</h4>
         <p>
-            This website lets you send out a comment
-        </p>
+            This website lets you comment ideas anonymously to your community.
+    </p>
+
+        <div>
+<h3>Questions, Comments, Concerns, Suggestions?  Let us know!</h3>
+        <form id="contact_form" action="#" method="POST" enctype="multipart/form-data">
+  
+  <div class="row">
+    <label class="required" for="message">Your message:</label><br />
+    <textarea id="message" class="input" name="message" rows="7" cols="30"></textarea><br />
+    <span id="message_validation" class="error_message"></span>
+  </div>
+    
+    <input id="submit_button" type="submit" value="Submit" />
+</form>
+</div>
         </>
     )
 }
