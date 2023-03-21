622e02906b0f79cc51b700edbd9b4422409c1fab
cerina Malaluan <cerinamalaluan@gmail.com>
2023-03-20 19:19:50 -0700

    logos

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+import logo1 from './Assests/ChattyKat.png'
+import logo2 from './Assests/logo.png'
+
+
+<><img src={logo1} alt="" /><img src={logo2} alt="" /></>
+
 function LandingP () {
 
     return(
