c73db714bac5be3d2a2dcd7decbed08733858c9a
cerina Malaluan <cerinamalaluan@gmail.com>
2023-03-20 19:19:14 -0700

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,10 @@
 // import logo from "../Assests/logo.png";
 // console.log(logo); // /logo.png
 import React from "react"
+import logo1 from './Assests/ChattyKat.png'
+import logo2 from './Assests/logo.png'
 
 
-function Header() {
-  // hopefully our cat logo
-  return (
-  <img src='./Assests/logo.png' alt="Logo" />
-  )
-}
+<><img src={logo1} alt="" /><img src={logo2} alt="" /></>
 
 export default Header
