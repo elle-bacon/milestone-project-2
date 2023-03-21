a1cc9847a721e28fc1d6727e55aec4fa917ca0b8
cerina Malaluan <cerinamalaluan@gmail.com>
2023-03-20 19:21:07 -0700

    logo

--- src/components/fqa.js
+++ src/components/fqa.js
@@ -1,4 +1,9 @@
 import React from "react"
+import logo1 from './Assests/ChattyKat.png'
+import logo2 from './Assests/logo.png'
+
+
+<><img src={logo1} alt="" /><img src={logo2} alt="" /></>
 
 function FQA () {
 
