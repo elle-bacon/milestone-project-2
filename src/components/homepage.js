2d84f6bac4bf2cd767c0efdb73e4b7c55c48a97c
cerina Malaluan <cerinamalaluan@gmail.com>
2023-03-27 18:27:13 -0700

    Revert "popup and css"
    This reverts commit 116699f237036a2053d8d167c27354000d1867b5.

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,27 +2,23 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Buttoncreate from "./button"
 
-// window. addEventListener("load", function(){
-//    function open(event){
-//         document.querySelector(".popup").style.display = "block";
-//     }
-//     setTimeout (open(),
-    
-//     2000
-
-//     )
-//     });
-//     document.querySelector("#close") .addEventListener
-//     ("click", function () {
-//     document.querySelector(".popup") .style.display =
-//     "none";
-//     }); 
-
-
-  
+window. addEventListener("load", function(){
+    setTimeout (
+    function open(event){
+    document.querySelector (" ‚popup").style.
+    display = "block";
+},
+    2000
+ 
+    )
+    });
+    document.querySelector ("#close") .addEventListener
+    ("click", function () {
+    document.querySelector (" popup") .style.display =
+    "none";
+    });
 
 function Chats(props) {
-    
     return (
         <>
             <div className="KATCHATS">
@@ -34,11 +30,9 @@ function Chats(props) {
 }
 
 function Homepage(props) {
-    const [chats, setChats] = useState({
+    let [chats, setChats] = useState({
         chats: [],
     })
-const [popupStyle, setPopupStyle] = useState("block")
-
 
     useEffect(() => {
         axios.get('http://localhost:5001/chats/')
@@ -62,21 +56,13 @@ const [popupStyle, setPopupStyle] = useState("block")
     return (
         <>
             <h3>Create a ChattyKat post:</h3>
-            <div style={{display:"flex", flexDirection:"column"}}>
-            <Buttoncreate id ="chatbutton"/>
+            <div>
+            <Buttoncreate />
 
                 {chatsList()}
             </div>
 
-            <div className="popup" style={{display:popupStyle}}>
-      <button id="close" onClick={()=>{setPopupStyle("none")}} >&times;</button>
-      <h3>agreement</h3>
-      <p>By using our site all users agree to refraining from illegal activity, hate speech and other forms of noninclusive behavior.  This website is intended for recreational purposes only.  Please fact check information you may read via posts and contact us if you this you have encountered a breach in our user agreement policy.  A contact link can be found under our FQA section.  Thank you for choosing to use ChattyKat.  ᵖᵘʳʳ^._.^= ∫
-      </p>
-    <button id="close" onClick={()=>{setPopupStyle("none")}} style={{fontSize:"10pt",color:"blue"}} >continue</button>
-       </div> 
-       </>
-
+        </>
     )
 }
 
